ffe7839029f64cc31995cced32de02c363e5e646
DrinoSan <s.becirbegovic@gmail.com>
2024-10-13 10:13:11 +0200

    Huston we got a moving texture

---
+++ Player.h
@@ -0,0 +1,44 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+// System Headers
+#include <stdint.h>
+
+// Project Headers
+#include "raylib.h"
+
+// TODO: move texture related stuff into own struct and use texture struct in
+// player struct
+struct Player_t
+{
+   char*     name;
+   int32_t   lifePoints;
+   Vector2   speed;
+   Rectangle rec;
+   Texture2D texture;
+   Vector2   texturePosition;
+   Rectangle textureFrameRec;
+   int       framesCounter;
+   int       framesSpeed;
+   int       currentFrame;
+};
+
+#define PLAYER_INIT( player, playerName )                                      \
+   do                                                                          \
+   {                                                                           \
+      player = ( struct Player_t ){ .name       = playerName,                  \
+                                    .lifePoints = 100,                         \
+                                    .speed      = { 0.0f, 0.0f },              \
+                                    .rec = { 350.0f, 280.0f, 50.0f, 50.0f },   \
+                                    .texture =                                 \
+                                        LoadTexture( "resources/scarfy.png" ), \
+                                    .texturePosition = { 350.0f, 280.0f },     \
+                                    .framesCounter   = 0,                      \
+                                    .framesSpeed     = 8,                      \
+                                    .currentFrame    = 0 };                       \
+      player.textureFrameRec =                                                 \
+          ( Rectangle ){ 0.0f, 0.0f, ( float ) player.texture.width / 6,       \
+                         ( float ) player.texture.height };                    \
+   } while ( false )
+
+#endif
--- main.c
+++ main.c
@@ -1,12 +1,13 @@
+#include "Player.h"
 #include "raylib.h"
-
 
 //------------------------------------------------------------------------------------
 // Global Variables Declaration
 //------------------------------------------------------------------------------------
 static const int screenWidth = 800;
-static const int screenHeight = 450;
+static const int screenHeight = 600;
 
+static struct Player_t player;
 
 //------------------------------------------------------------------------------------
 // Module Functions Declaration (local)
@@ -17,15 +18,14 @@ static void DrawGame(void);         // Draw game (one frame)
 static void UnloadGame(void);       // Unload game
 static void UpdateDrawFrame(void);  // Update and Draw (one frame)
 
-
-//------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 // Program main entry point
-//------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 int main(void)
 {
     // Initialization (Note windowTitle is unused on Android)
     //---------------------------------------------------------
-    InitWindow(screenWidth, screenHeight, "classic game: space invaders");
+    InitWindow( screenWidth, screenHeight, "Drive and Fight people" );
 
     InitGame();
 
@@ -35,10 +35,9 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update and Draw
-        //----------------------------------------------------------------------------------
-        UpdateDrawFrame();
-        //----------------------------------------------------------------------------------
+       //----------------------------------------------------------------------------------
+       UpdateDrawFrame();
+       //----------------------------------------------------------------------------------
     }
     // De-Initialization
     //--------------------------------------------------------------------------------------
@@ -50,23 +49,69 @@ int main(void)
     return 0;
 }
 
+//-----------------------------------------------------------------------------
 void InitGame(void)
 {
+   PLAYER_INIT( player, "Sandrino" );
 }
 
+//-----------------------------------------------------------------------------
 // Update game (one frame)
 void UpdateGame(void)
 {
+   // Update and Draw
+   // Handling of player sprite
+   //----------------------------------------------------------------------------------
+   player.framesCounter++;
+
+   if ( player.framesCounter >= ( 60 / player.framesSpeed ) )
+   {
+      player.framesCounter = 0;
+      player.currentFrame++;
+
+      if ( player.currentFrame > 5 )
+         player.currentFrame = 0;
+
+      player.textureFrameRec.x =
+          ( float ) player.currentFrame * ( float ) player.texture.width / 6;
+   }
+
+   // Control frames speed
+   if ( IsKeyPressed( KEY_RIGHT ) )
+      player.framesSpeed++;
+   else if ( IsKeyPressed( KEY_LEFT ) )
+      player.framesSpeed--;
 }
 
+//-----------------------------------------------------------------------------
 void DrawGame(void)
 {
+   BeginDrawing();
+
+   ClearBackground( RAYWHITE );
+
+   DrawText( "Petrol", screenWidth / 2 - MeasureText( "Petrol", 40 ) / 2,
+             screenHeight / 2 - 40, 40, BLACK );
+
+   DrawTextureRec( player.texture, player.textureFrameRec,
+                   player.texturePosition,
+                   WHITE );   // Draw part of the texture
+
+   DrawText( "(c) Scarfy sprite by Eiden Marsal", screenWidth - 200,
+             screenHeight - 20, 10, GRAY );
+
+   EndDrawing();
 }
 
+//-----------------------------------------------------------------------------
 void UnloadGame(void)
 {
+   UnloadTexture( player.texture );
 }
 
+//-----------------------------------------------------------------------------
 void UpdateDrawFrame(void)
 {
+   UpdateGame();
+   DrawGame();
 }
